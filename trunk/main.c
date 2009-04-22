7c1a3e8569bd2cdb4c7e5bf6b7b8080f452bbc45
Kaiznb <Kaiznb@d9131640-2e5e-11de-acd2-81e04a9d018a>
2009-04-22 20:41:20 +0000

    Ajout module main

---
+++ trunk/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <SDL/SDL.h>
+#include "menu.h"
+#include "gestionCircuit.h"
+
+int main(int argc, char *argv[]) {
+	Uint32 initflags = SDL_INIT_VIDEO;  //See documentation for details
+	SDL_Surface *ecran;
+	Partie *partie;
+	Uint8  video_bpp = 32;
+	Uint32 videoflags = SDL_HWSURFACE | SDL_DOUBLEBUF;
+
+
+
+	
+	// Initialize the SDL library
+	if ( SDL_Init(initflags) < 0 ) {
+		fprintf(stderr, "Couldn't initialize SDL: %s\n",
+				SDL_GetError());
+		exit(1);
+	}
+	
+	// Set 640x480 video mode 
+	ecran=SDL_SetVideoMode(800,600, video_bpp, videoflags);
+    if (ecran == NULL) {
+		fprintf(stderr, "Couldn't set 640x480x%d video mode: %s\n",
+				video_bpp, SDL_GetError());
+		SDL_Quit();
+		exit(2);
+	}
+	
+	SDL_WM_SetCaption("Jeu de course de voitures en 2D !", NULL);
+	
+	
+	gestionCircuit(ecran, partie);
+	//menuAccueil(ecran, partie);
+		
+	SDL_FreeSurface(ecran);
+	
+
+	// Clean up the SDL library 
+	SDL_Quit();
+	return(0);
+}
+
+
