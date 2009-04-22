8e5b2cc7a8c6d4393a1992904c57aadb51fdd1e5
Kaiznb <Kaiznb@d9131640-2e5e-11de-acd2-81e04a9d018a>
2009-04-22 20:43:25 +0000

    Ajout de la compatibilité pour le modèle

--- trunk/affichage.c
+++ trunk/affichage.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL/SDL.h>
+#include "menu.h"
 #include "gestionCircuit.h"
 #include "affichage.h"
 #include "camera.h"
 
 
-void affichage(SDL_Surface *ecran, Voiture voiture, SDL_Surface **sprite, char ***circuit1,int coin[],int coinprec[], SDL_Rect *position, SDL_Surface *fond[]){
+void affichage(SDL_Surface *ecran, Voiture voiture, SDL_Surface **sprite, Circuit circuit,int coin[],int coinprec[], SDL_Rect *position, SDL_Surface *fond[]){
 	voiture.image=sprite[(voiture.angle)];
-	camera (ecran, circuit1, position, 0, 0, coin, coinprec, fond);
+	camera (ecran, circuit, position, 0, 0, coin, coinprec, fond);
 	if(voiture.position.y>=600-100) 
-		camera (ecran, circuit1, position, 0, -(int)(voiture.vitesse.y), coin, coinprec, fond);
+		camera (ecran, circuit, position, 0, -(int)(voiture.vitesse.y), coin, coinprec, fond);
 	if(voiture.position.x>=800-100) 
-		camera (ecran, circuit1, position, -(int)(voiture.vitesse.x), 0, coin, coinprec, fond);
+		camera (ecran, circuit, position, -(int)(voiture.vitesse.x), 0, coin, coinprec, fond);
 	if(voiture.position.y<=100)
-		camera (ecran, circuit1, position, 0, (int)voiture.vitesse.y, coin, coinprec, fond);
+		camera (ecran, circuit, position, 0, (int)(voiture.vitesse.y), coin, coinprec, fond);
 	if(voiture.position.x<=100)
-		camera (ecran, circuit1, position, (int)voiture.vitesse.x, 0, coin, coinprec, fond);
+		camera (ecran, circuit, position, (int)(voiture.vitesse.x), 0, coin, coinprec, fond);
 	SDL_BlitSurface(voiture.image, NULL, ecran, &voiture.position);
 	SDL_Flip(ecran);
 }
--- trunk/gestionCircuit.c
+++ trunk/gestionCircuit.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <SDL/SDL.h>
-#include <SDL/SDL_rotozoom.h>
+#include "menu.h"
 #include "gestionCircuit.h"
 #include "physique.h"
 #include "affichage.h"
+
 
 int allocationVoiture (SDL_Surface ***surface, char nomVoiture[]) {
 	int i;
@@ -19,14 +20,13 @@ int allocationVoiture (SDL_Surface ***surface, char nomVoiture[]) {
 		}
 		SDL_SetColorKey((*surface)[i], SDL_SRCCOLORKEY, SDL_MapRGB((*surface)[i]->format, 97, 68, 43));
 	}
-	
 	return 0;
 }
 
 
-int initialisation (SDL_Surface ***spriteVoitures, Voiture voitures[], Circuit * circuit, int nbrDeJoueurs, int coin[], int coinprec[]) {
+int initialisation (SDL_Surface *** spriteVoitures, Voiture voitures[], Circuit * circuit, int nbrDeJoueurs, int coin[], int coinprec[]) {
 	int i,j;
-	char ***tab;	
+	char *** tab;	
 	
 	//initialisation des voitures 
 	for (i=0; i<nbrDeJoueurs; i++) 
@@ -45,13 +45,13 @@ int initialisation (SDL_Surface ***spriteVoitures, Voiture voitures[], Circuit *
 	circuit->largeurImage=1024;
 	circuit->hauteurImage=768;
 	
-	tab=(char ***)calloc(2,sizeof(char**)); 
+	tab=(char ***)calloc(circuit->nbrImageX,sizeof(char**)); 
 
-	for(i=0;i<2;i++)  {
-		tab[i]=(char **) calloc( 2 ,sizeof(char*));
-		for(j=0;j<2;j++) {
+	for(i=0;i<circuit->nbrImageX;i++)  {
+		tab[i]=(char **) calloc( circuit->nbrImageY ,sizeof(char*));
+		for(j=0;j<circuit->nbrImageY;j++) {
 			tab[i][j]=(char *) calloc( 1024 ,sizeof(char));
-			strcpy_s(tab[i][j],14,"Circuit\\1.bmp");
+			strcpy(tab[i][j],"Circuit/3.bmp");
 		}
 	}
 	circuit->image=tab;
@@ -66,51 +66,21 @@ int initialisation (SDL_Surface ***spriteVoitures, Voiture voitures[], Circuit *
 	return 0;
 }
 
-
-int main(int argc, char *argv[]) {
-	Uint32 initflags = SDL_INIT_VIDEO;  //See documentation for details
-	SDL_Surface *ecran;
-	Uint8  video_bpp = 32;
-	Uint32 videoflags = SDL_HWSURFACE | SDL_DOUBLEBUF;
-	int done,i;
-	int tempsPrecedent = 0, tempsActuel = 0;
-	int nbrDeJoueurs = 1;
-
-	SDL_Event event;
-	SDL_Rect position;
+int gestionCircuit( SDL_Surface *ecran, Partie *partie) {
 	
-	SDL_Surface ***sprite;// ={NULL} ;
-	SDL_Surface * fond[4];
+	SDL_Rect position;
+	int    done,i;
+	int tempsPrecedent = 0, tempsActuel = 0;
+	
+	SDL_Event event;
 
+	int nbrDeJoueurs = 1;
+	SDL_Surface * fond[4];
+	SDL_Surface *** sprite = malloc(nbrDeJoueurs * sizeof(SDL_Surface **));
 	Voiture voitures[1];
 	Circuit circuit;
 	int coin[2];
 	int coinprec[2];
-
-	
-	if((sprite=malloc(nbrDeJoueurs * sizeof(SDL_Surface **)))==NULL){
-	return 1;
-	}
-
-	// Initialize the SDL library
-	if ( SDL_Init(initflags) < 0 ) {
-		fprintf(stderr, "Couldn't initialize SDL: %s\n",
-				SDL_GetError());
-		exit(1);
-	}
-	
-	// Set 800x600 video mode 
-	ecran=SDL_SetVideoMode(800,600, video_bpp, videoflags);
-    if (ecran == NULL) {
-		fprintf(stderr, "Couldn't set 800x600 video mode: %s\n",
-				video_bpp, SDL_GetError());
-		SDL_Quit();
-		exit(2);
-	}
-	
-	SDL_WM_SetCaption("Jeu de course de voitures en 2D !", NULL);
-	
-	
 	
 	position.x = 280;
 	position.y = 280;
@@ -129,14 +99,13 @@ int main(int argc, char *argv[]) {
 		if (tempsActuel - tempsPrecedent > 30){
 			tempsPrecedent = tempsActuel;
 			deplacer(voitures);
-			affichage(ecran,voitures[0],sprite[0],circuit.image,coin,coinprec,&position,fond);
+			affichage(ecran,voitures[0],sprite[0],circuit,coin,coinprec,&position,fond);
 			SDL_PollEvent(&event);
 			if(event.type==SDL_KEYDOWN){
 				if((event.key.keysym.sym)==SDLK_UP) voitures[0].haut=1;
 				if((event.key.keysym.sym)==SDLK_DOWN) voitures[0].bas=1;
 				if((event.key.keysym.sym)==SDLK_LEFT) voitures[0].gauche=1;
 				if((event.key.keysym.sym)==SDLK_RIGHT) voitures[0].droite=1;
-				if((event.key.keysym.sym)==SDLK_ESCAPE)done = 1;
 			}
 			if(event.type==SDL_KEYUP){
 				if((event.key.keysym.sym)==SDLK_UP)voitures[0].haut=0;
@@ -153,15 +122,11 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	
-	SDL_FreeSurface(ecran);
+	for(i=0; i<32;i++)
+		SDL_FreeSurface(sprite[0][i]);
 	
-//	for(i=0; i<33;i++)
-//		SDL_FreeSurface(sprite[0][i]);
-	 
 	for(i=0; i<4;i++)
 		SDL_FreeSurface(fond[i]);
-	// Clean up the SDL library */
-	SDL_Quit();
-	return(0);
+	
+	return 0;
 }
-
