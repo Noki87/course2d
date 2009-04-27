2f2b485e4e586c4384630a9f7ef67dcf7030fdd9
noki.sauvage <noki.sauvage@d9131640-2e5e-11de-acd2-81e04a9d018a>
2009-04-27 21:44:45 +0000

    Ca compile mais problème d'initialisation de "texte" dans menu.c

--- trunk/affichage.c
+++ trunk/affichage.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL/SDL.h>
-#include <SDL/SDL_ttf.h>
-#include "menu.h"
+//#include <SDL/SDL_ttf.h>
 #include "gestionCircuit.h"
 #include "affichage.h"
 #include "camera.h"
+#include "menu.h"
 
 
 void affichage(SDL_Surface *ecran, Voiture voiture, SDL_Surface **sprite, Circuit circuit,int coin[],int coinprec[], SDL_Rect *position, SDL_Surface *fond[]){
-        voiture.image=sprite[(voiture.angle)];
-        camera (ecran, circuit, position, 0, 0, coin, coinprec, fond);
-        if(voiture.position.y>=600-100)
-                camera (ecran, circuit, position, 0, -(int)(voiture.vitesse.y), coin, coinprec, fond);
-        if(voiture.position.x>=800-100)
-                camera (ecran, circuit, position, -(int)(voiture.vitesse.x), 0, coin, coinprec, fond);
-        if(voiture.position.y<=100)
-                camera (ecran, circuit, position, 0, (int)(voiture.vitesse.y), coin, coinprec, fond);
-        if(voiture.position.x<=100)
-                camera (ecran, circuit, position, (int)(voiture.vitesse.x), 0, coin, coinprec, fond);
-        SDL_BlitSurface(voiture.image, NULL, ecran, &voiture.position);
+        voiture.image=sprite[(voiture.angle)];  
+        camera (ecran, circuit, voiture, fond, coin, coinprec, position);
+        SDL_BlitSurface(voiture.image, NULL, ecran, position);
         SDL_Flip(ecran);
+
 }
 
