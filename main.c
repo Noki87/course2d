bdb0b8a901ce094181c3c3aaf74545630f40d404
Kaiznb <Kaiznb@d9131640-2e5e-11de-acd2-81e04a9d018a>
2009-05-01 13:22:30 +0000

    Refonte du module menu.c pour éviter la redondance de code avec l'ajout de la fonction gestion menu. La fonction saisir marche mais il reste un bug pas encore identifier lorsqu'on modifie le nom du joueur le programme plante parfois. Il est sûrement du à une fuite de mémoire.

--- main.c
+++ main.c
@@ -3,58 +3,63 @@
 #include <string.h>
 #include <math.h>
 #include <SDL/SDL.h>
+#ifdef __APPLE__
+#include <SDL_ttf/SDL_ttf.h>
+#else
 #include <SDL/SDL_ttf.h>
+#endif
 #include "menu.h"
 #include "gestionCircuit.h"
 
 int main(int argc, char *argv[]) {
-        Uint32 initflags = SDL_INIT_VIDEO;  //See documentation for details
-        SDL_Surface *ecran;
-        Partie *partie;
-        Uint8  video_bpp = 32;
-        Uint32 videoflags = SDL_HWSURFACE | SDL_DOUBLEBUF;
-
-        partie=malloc(sizeof(Partie));
-
-
-        
-        // Initialize the SDL library
-        if ( SDL_Init(initflags) < 0 ) {
-                fprintf(stderr, "Couldn't initialize SDL: %s\n",
-                                SDL_GetError());
-                exit(1);
-        }
-
-		 //Initialize the SDL_ttf library
-        if(TTF_Init() < 0 ){
+	Uint32 initflags = SDL_INIT_VIDEO;  //See documentation for details
+	SDL_Surface *ecran;
+	Partie *partie;
+	Uint8  video_bpp = 32;
+	Uint32 videoflags = SDL_HWSURFACE | SDL_DOUBLEBUF;
+	
+	partie=malloc(sizeof(Partie));
+	
+	
+	
+	// Initialize the SDL library
+	if ( SDL_Init(initflags) < 0 ) {
+		fprintf(stderr, "Couldn't initialize SDL: %s\n",
+				SDL_GetError());
+		exit(1);
+	}
+	
+	//Initialize the SDL_ttf library
+	if(TTF_Init() < 0 ){
 		fprintf(stderr, "Erreur d'initialisation de TTF_Init : %s\n", 
-								TTF_GetError());
-				exit(1);
-}
-
-
-        
-        // Set 640x480 video mode 
-        ecran=SDL_SetVideoMode(800,600, video_bpp, videoflags);
+				TTF_GetError());
+		exit(1);
+	}
+	
+	
+	
+	// Set 640x480 video mode 
+	ecran=SDL_SetVideoMode(800,600, video_bpp, videoflags);
     if (ecran == NULL) {
-                fprintf(stderr, "Couldn't set 640x480x%d video mode: %s\n",
-                                video_bpp, SDL_GetError());
-                SDL_Quit();
-                exit(2);
-        }
-        
-        SDL_WM_SetCaption("Jeu de course de voitures en 2D !", NULL);
-        
-        initialiserPartie(partie);
-        
-        menuAccueil(ecran, partie);
+		fprintf(stderr, "Couldn't set 640x480x%d video mode: %s\n",
+				video_bpp, SDL_GetError());
+		SDL_Quit();
+		exit(2);
+	}
+	
+	SDL_WM_SetCaption("Jeu de course de voitures en 2D !", NULL);
+	
+	initialiserPartie(partie);
+	
+	gestionMenu(ecran, partie);
+	if(partie->menu == MenuJeu)
         gestionCircuit(ecran, partie);  
-
-        SDL_FreeSurface(ecran);
-        
-
-        // Clean up the SDL library 
-		TTF_Quit();
-        SDL_Quit();
-        return(0);
+	
+	SDL_FreeSurface(ecran);
+	
+	
+	// Clean up the SDL library 
+	TTF_Quit();
+	SDL_Quit();
+	return(0);
 }
