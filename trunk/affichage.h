13d137406352e17e7fdda458f2c6ec0b68e977d7
Kaiznb <Kaiznb@d9131640-2e5e-11de-acd2-81e04a9d018a>
2009-05-19 17:02:39 +0000

    Ajout de commentaire.

--- trunk/affichage.h
+++ trunk/affichage.h
@@ -1,7 +1,22 @@
 #ifndef AFFICHAGE_H
 #define AFFICHAGE_H
+
+/*
+	La fonction affichage prend en paramètre le SDL_surface qui gère l'écran, le tableau des voitures, les données du circuit et de la caméra ainsi que le nombre de joueurs
+	Elle blit sur l'écran le fond, les voitures, le temps depuis le début de la course, le nombre de tour en mode 1J et le nombre de points en mode 2J. 
+*/
 void affichage(SDL_Surface *ecran, Voiture *voiture, Circuit circuit, Camera *camera, int nbrDeJoueurs);
-int affichageDecompte(SDL_Surface *ecran, Camera *camera, int *compteur, int *tempsPrec, int tempsPause);
+
+/*
+	La fonction affichageDecompte prend en paramètre le SDL_surface qui gère l'écran, les données de la caméra, un compteur qui est décrémenter toute les secondes, 
+	le tempsPrécédent et le tempsPause qui serve à savoir si une seconde s'est écoulée
+	Elle blit sur l'ecran le décompte.
+*/ 
+int affichageDecompte(SDL_Surface *ecran, Camera *camera, int *compteur, int *tempsPrecedent, int tempsPause);
+
+/*
+	Affiche un écran de chargement : fond noir avec écris chargement en blanc
+ */
 int ecranChargement (SDL_Surface * ecran);
 
 #endif
--- trunk/camera.h
+++ trunk/camera.h
@@ -1,5 +1,18 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 #include <SDL/SDL.h>
+/*
+	La fonction positioonerCamera prend en paramètre le SDL_surface qui gère l'écran, les données du circuit, de la voiture et de la caméra
+	Elle blit le fond sur l'écran. La position du fond est calculée pour centrer la voiture au milieu de l'écran sauf lorsqu'on se trouve sur les bords du circuit. 
+	A ce moment là, la voiture bouge sur l'écran alors que le fond reste collé sur le bord.
+*/
 void positionnerCamera (SDL_Surface *ecran, Circuit circuit, Voiture voiture, Camera * camera);
+
+/*
+ La fonction positionnerCamera prend en paramètre le SDL_surface qui gère l'écran, les données du circuit, de la voiture et de la caméra
+ Elle blit le fond sur l'écran. La position du fond est calculée pour que le baricentre entre les deux voitures soit centré au milieu de l'écran sauf lorsqu'on se trouve sur les bords du circuit. 
+ A ce moment là, les voiture bouge sur l'écran alors que le fond reste collé sur le bord.
+ Le barycentre se raproche de la voiture de tête lorsque la distance entre les voitures augmentent.
+ */
+void positionnerCamera2j (SDL_Surface *ecran, Circuit circuit, Voiture *voiture, Camera * camera);
 #endif
